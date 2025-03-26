adae0510ef125e109c3fd2da72a7d376393ee608
thyszcl <thyszcl@gmail.com>
2025-03-26 21:03:53 +0800

    Delete site.js

--- site.js
+++
@@ -1,6 +0,0 @@
-function sendMail(){
-    let msg = {
-        message: [document.getElementById('formemailinp').value, document.getElementById('formnameinp').value, document.getElementById('formtxtarea').value ]
-    }
-    emailjs.send('service_0s5fge5', "template_9nzmud8",msg).then(alert('Email Sent!'))
-}
